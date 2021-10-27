2e10e003408efc1242058991c4d46975738851ed
gabrielbarrosba <77939255+gabrielbarrosba@users.noreply.github.com>
2021-10-27 16:43:27 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-window.onscroll=function(){rolagemCabecalho()};
+const btnMoblie = document.getElementById('btn-mobile');
 
-function rolagemCabecalho(){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        document.getElementsByTagNameNS("header")[0].style.padding= "30px 10px";
-        document.getElementById("logo").style.fontSize="25px";
-    }else{
-        document.getElementsByTagNameNS("header")[0].style.padding= "80px 1px";
-        document.getElementById("logo").style.fontSize="35px";
-    }
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active')
 }
+
+btnMoblie.addEventListener('click', toggleMenu);
